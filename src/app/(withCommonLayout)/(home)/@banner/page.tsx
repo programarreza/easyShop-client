f8bdd1d943ad9a55b4b73e7a9e93fb0688da8e1c
programarreza <programarreza@gmail.com>
2024-12-14 15:13:10 +0600

    added to banner section

--- src/app/(withCommonLayout)/(home)/@banner/page.tsx
+++ src/app/(withCommonLayout)/(home)/@banner/page.tsx
@@ -1,15 +1,53 @@
-import Container from "@/src/components/ui/Container";
+"use client";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import Image from "next/image";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 const Banner = () => {
   return (
-    <Container>
-      <div className="min-h-[90vh] border border-red-500">
-        <p className="flex justify-center items-center mt-24">
-          {" "}
-          this is banner area
-        </p>
-      </div>
-    </Container>
+    <div className="h-[650px] w-full ">
+      <Swiper
+        autoplay={{
+          delay: 4500,
+          disableOnInteraction: false,
+        }}
+        centeredSlides={false}
+        className="mySwiper h-full"
+        modules={[Autoplay, Pagination, Navigation]}
+        navigation={false}
+        pagination={{
+          clickable: true,
+        }}
+        spaceBetween={30}
+      >
+        <SwiperSlide>
+          <Image
+            alt="banner image"
+            className=" h-full w-full"
+            height={500}
+            src="https://res.cloudinary.com/dudjn6epk/image/upload/v1734165428/vqq76z0kxztjkfjakzvp.webp"
+            width={1000}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="banner image"
+            className=" h-full w-full"
+            height={500}
+            src="https://res.cloudinary.com/dudjn6epk/image/upload/v1734165434/esu07maf89zigc64h8er.webp"
+            width={1000}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
